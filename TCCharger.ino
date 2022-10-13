07d140b702684507f7609021c8a670175ea19225
Jørn Moe <jorn.moe@basefarm.com>
2022-10-13 12:39:03 +0200

    Work in progress

--- TCCharger.ino
+++ TCCharger.ino
@@ -1,34 +1,45 @@
-//Code von Lennart O.
+//Based on Code from Lennart O.
 
-//Bibliotheken einbinden
-
+//
+//#include <CAN.h>
 #include <mcp_can.h> 
 #include <SPI.h>
 #include <SimpleTimer.h>
 
-//Konstanten und Variablen deklarieren
+// #define Serial Serial1 // uncomment if you use Thinary Nano every 4808 "USB serial hack"
+#define SPI_CS_PIN 8 //CS Pin 
 
-#define SPI_CS_PIN 10 //CS Pin
-#define POTI 0 //A0 für Poti
+// Potentiometers should be connected to +5V, anlog pin and GND.
+#define AMP_ADJ 0 //Potentiometer connected to A0 for adjusting charge current
+#define VOLT_ADJ 1 //Potentiometer connected to A1 for setting target SOC voltage 
+#define MAX_VOLTAGE 1162 // set max voltage to 116,2V (offset = 0,1)
 
-word outputvoltage = 1162; //setze max Spannung auf 116,2V (offset = 0,1)
-word outputcurrent = 320; //setze max Strom auf 32A (offset = 0,1)
-float poti = 0;
+//***********************************************************
+// select max charge current based on which charger you have (offset = 0,1)
+#define MAX_CURRENT 160 // set max charge current to 16A (TC1800)
+//#define MAX_CURRENT 320 // set max charge current to 32A (TC3300)
+//***********************************************************
+
+word outputcurrent = 0; // initial current when starting up
+word outputvoltage = MAX_VOLTAGE; // set initial voltage to MAX_VOLTAGEV (offset = 0,1)
+
+float ampadj = 0;
+float voltadj = MAX_VOLTAGE;
 
 unsigned long int sendId = 0x1806E5F4;
 unsigned char voltamp[8] = {highByte(outputvoltage), lowByte(outputvoltage), highByte(outputcurrent), lowByte(outputcurrent), 0x00,0x00,0x00,0x00};
 
-unsigned char len = 0; //Länge emfpangener CAN Nachricht
-unsigned char buf[8]; //Buffer für Daten aus CAN Nachricht
-unsigned long int receiveId; //ID des Senders
+unsigned char len = 0; // Length of CAN recieve message 
+unsigned char buf[8]; //Buffer for data from CAN message
+unsigned long int receiveId; //Senders ID 
 
-//Objekte deklarieren
+//Object declarations 
 
-MCP_CAN CAN(SPI_CS_PIN); //CS Pin für SPI setzen
+MCP_CAN CAN(SPI_CS_PIN); //Set CS Pin for SPI
 
-SimpleTimer timer1; //timer Objekt erzeugen
+SimpleTimer timer1; //Provide timer Object
 
-//Funktionen
+//Functions
 
 /************************************************
 ** Function name:           canRead
@@ -37,49 +48,49 @@ SimpleTimer timer1; //timer Objekt erzeugen
 
 void canRead(){
 
-  if(CAN_MSGAVAIL == CAN.checkReceive()){ //auf Nachrichten prüfen
+  if(CAN_MSGAVAIL == CAN.checkReceive()){ //check for message
 
     CAN.readMsgBuf(&len, buf); // read data, len: data length, buf: data buffer
 
-    receiveId = CAN.getCanId(); //CAN-ID lesen
+    receiveId = CAN.getCanId(); //Read CAN-ID
 
-    if(receiveId == 0x18FF50E5){ //CAN Bus ID vom Ladegerät
+    if(receiveId == 0x18FF50E5){ //CAN Bus ID from charger
 
-      Serial.println("CAN Daten vom Ladegerät empfangen!");
+      Serial.println("CAN Data from charger received!");
 
       Serial.print("CAN ID: ");
-      Serial.print(receiveId, HEX); //ID ausgeben
+      Serial.print(receiveId, HEX); //Print ID
 
-      Serial.print(" / CAN Daten: ");
-      for(int i = 0; i<len; i++){ //Daten ausgeben
+      Serial.print(" / CAN data: ");
+      for(int i = 0; i<len; i++){ // print the data
 
-        if( buf[i] < 0x10){ // führende Null wenn nur eine Ziffer
+        if( buf[i] < 0x10){ // add leading zero if only one digit value
           Serial.print("0");
         }
 
         Serial.print(buf[i],HEX);
-        Serial.print(" ");          // Leerzeichen
+        Serial.print(" ");          // Space
 
       }
 
-      Serial.println(); //Absatz
+      Serial.println(); //New line
 
-      Serial.print("Ladespannung: ");
+      Serial.print("Charge voltage: ");
       float pv_voltage = (((float)buf[0]*256.0) + ((float)buf[1]))/10.0; //highByte/lowByte + offset
       Serial.print(pv_voltage);
-      Serial.print(" V / Ladestrom: ");
+      Serial.print(" V / Charge current: ");
       float pv_current = (((float)buf[2]*256.0) + ((float)buf[3]))/10.0; //highByte/lowByte + offset
       Serial.print(pv_current);
-      Serial.println(" A"); //Absatz
+      Serial.println(" A"); //New line
 
-      switch (buf[4]) { //Fehlerbyte auslesen
+      switch (buf[4]) { // Read error code
 
-        case B00000001: Serial.println("Fehler: Hardwarefehler");break;
-        case B00000010: Serial.println("Fehler: Übertemperatur");break;
-        case B00000100: Serial.println("Fehler: Eingangsspannung unzulässig");break;
-        case B00001000: Serial.println("Fehler: Batterie nicht verbunden");break;
-        case B00010000: Serial.println("Fehler: CAN-Bus Fehler");break;
-        case B00001100: Serial.println("Fehler: Keine Eingangsspannung");break;
+        case 0b0000001: Serial.println("Error: Hardware fail");break;
+        case 0b0000010: Serial.println("Error: Overheating");break;
+        case 0b0000100: Serial.println("Error: Mains voltage out of spec.");break;
+        case 0b0001000: Serial.println("Error: Battery not connected");break;
+        case 0b0010000: Serial.println("Error: CAN-Bus error");break;
+        case 0b0001100: Serial.println("Error: Mains disconnected");break;
 
       }
 
@@ -96,12 +107,12 @@ void canRead(){
 
 String canWrite(unsigned char data[8], unsigned long int id){
 
-  byte sndStat = CAN.sendMsgBuf(id, 1, 8, data); //Nachricht senden (ID, extended Frame, Datenlänge, Daten)
+  byte sndStat = CAN.sendMsgBuf(id, 1, 8, data); // Send message (ID, extended Frame, data lenght, Data)
 
-  if(sndStat == CAN_OK) //Statusbyte für Übertragung
-    return "CAN Nachricht erfolgreich gesendet";
+  if(sndStat == CAN_OK) //Statusbyte for transmission
+    return "CAN message transmission OK";
   else
-    return "Fehler bei Nachrichtenübertragung";
+    return "Transmission error";
 
 }
 
@@ -112,11 +123,34 @@ String canWrite(unsigned char data[8], unsigned long int id){
 
 void setVoltage(int t_voltage) { //can be used to set desired voltage to i.e. 80% SOC
 
-  if(t_voltage >= 980 && t_voltage <= 1164){
+  if(t_voltage >= 980 && t_voltage <= MAX_VOLTAGE){
     
     outputvoltage = t_voltage;
     
   }
+
+ }
+
+/************************************************
+** Function name:           readVoltAdj
+** Descriptions:            read voltadj to adjust voltage
+*************************************************/
+
+void readVoltAdj() {
+// 99,12 - 116,2 
+
+  ampadj = analogRead(VOLT_ADJ); // Read value from VoltAdj potentiometer connected to A1
+  Serial.println(voltadj);
+  // some clever math to have minimum target voltage = 99.1V and max target voltage = MAX_VOLTAGE corresponding to target SOC from aproximatly 35% to 100%. 
+  // Note the scale isn't linear so marking the potentiometer should be done by setting a position and take note of SOC when charging stops
+  int voltage = (int) (MAX_VOLTAGE*((voltadj+5934)/6958));
+    
+  if(voltage > MAX_VOLTAGE){
+
+   voltage = MAX_VOLTAGE; // ensure that charge voltage doesn't go over MAX_VOLTAGE
+
+  }  
+  setVoltage(voltage); 
 
  }
 
@@ -127,7 +161,7 @@ void setVoltage(int t_voltage) { //can be used to set desired voltage to i.e. 80
 
 void setCurrent(int t_current) { //can be used to reduce or adjust charging speed
 
-  if(t_current >= 0 && t_current <= 320){
+  if(t_current >= 0 && t_current <= MAX_CURRENT){
     
     outputcurrent = t_current;
     
@@ -136,21 +170,21 @@ void setCurrent(int t_current) { //can be used to reduce or adjust charging spee
  }
 
 /************************************************
-** Function name:           readPoti
-** Descriptions:            read poti to adjust current
+** Function name:           readAmpAdj
+** Descriptions:            read ampadj to adjust current
 *************************************************/
 
-void readPoti() {
+void readAmpAdj() {
 
-  poti = analogRead(POTI); //Wert vom Poti einlesen
-  int current = (int) (320.0*(poti/850.0)) //keine 1024 erreicht, daher 850
+  ampadj = analogRead(AMP_ADJ); // Read value from AmpAdj potentiometer connected to A0
+  Serial.println(ampadj);
+  int current = (int) (MAX_CURRENT*((ampadj+200)/1120.0)); // some clever math to have minimum charge curret > 0A.
     
-  if(current > 320){
+  if(current > MAX_CURRENT){
 
-   current = 320; //begrenze auf 320 falls Ergebnis größer ist
+   current = MAX_CURRENT; // ensure that charge current doesn't go over MAX_CURRENT
 
   }  
- 
   setCurrent(current); 
 
  }
@@ -160,18 +194,19 @@ void readPoti() {
 ** Descriptions:            function of timer1
 *************************************************/
 
-void myTimer1() { //zyklisch vom Timer aufgerufene Funktion
+void myTimer1() { // Function called repeatedly by the timer
 
-  Serial.print("Eingestellter Ladestrom: ");
-  Serial.print((float)outputcurrent/10.0); //Current setpoint ausgeben
+  Serial.print("Charge current setting: ");
+  Serial.print((float)outputcurrent/10.0); // Print the current setting
   Serial.println(" A");
   
-  unsigned char voltamp[8] = {highByte(outputvoltage), lowByte(outputvoltage), highByte(outputcurrent), lowByte(outputcurrent), 0x00,0x00,0x00,0x00}; //Nachricht neu generieren
-  Serial.println(canWrite(voltamp, sendId)); //Nachricht senden und Ergebnis ausgeben
+  unsigned char voltamp[8] = {highByte(outputvoltage), lowByte(outputvoltage), highByte(outputcurrent), lowByte(outputcurrent), 0x00,0x00,0x00,0x00}; // Prepare new CAN bus message
+  Serial.println(canWrite(voltamp, sendId)); // Send message and print the result
   
-  canRead(); //Lesefunktion aufrufen
-  
-  Serial.println(); //Absatz
+  canRead(); // Call the read CAN function
+  readAmpAdj(); // Read the current adjust potentiometer (comment this out if you don't have a current adjust potentiometer connected)
+  readVoltAdj(); // Read the voltage adjust potentiometer (comment this out if you don't have a voltage adjust potentiometer connected)
+  Serial.println(); //New line
 
 }
 
@@ -182,18 +217,18 @@ void myTimer1() { //zyklisch vom Timer aufgerufene Funktion
 
 void setup() {
 
-  Serial.begin(115200); //Serielle Schnittstelle starten
+  Serial.begin(115200); // Start serial interface
 
-  while(CAN_OK != CAN.begin(CAN_250KBPS, MCP_8MHz)){ //CAN Bus initialisieren
+  while(CAN_OK != CAN.begin(CAN_250KBPS, MCP_8MHz)){ //CAN Bus initialization
 
-    Serial.println("CAN Initialisierung fehlgeschlagen, Neustart");
+    Serial.println("CAN initialization error, Retrying...");
     delay(200);
 
   }
 
   Serial.println("CAN Initialisierung erfolgreich");
 
-  timer1.setInterval(950, myTimer1); //Zeit und Funktion des Timers definieren
+  timer1.setInterval(950, myTimer1); // Configre the timer function
 
 }
 
@@ -204,6 +239,6 @@ void setup() {
 
 void loop() {
 
-  timer1.run(); //Timer starten
+  timer1.run(); //Start the timer function
 
 }
